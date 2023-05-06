d92cbeea954e161cb8cdf20324acb0cc0cac0149
Victoria <vmcnorrill@gmail.com>
2023-05-06 15:52:28 -0400

    ERROR: Refused to execute script from 'http://localhost:3001/public/js/checkbox.js' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.

--- public/js/checkbox.js
+++ public/js/checkbox.js
@@ -1,17 +1,26 @@
 // if user checks at least one input from the list,
 // then all "required" attr are removed
 
-let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-
 function reqCheckOne(){
-    // if a checkbox is checked, function ends and returns true.
-    for (let i = 0; i < checkboxes.length; i++){
+    //select input checkbox from doc
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    // if a checkbox is checked, all required attr are removed.
+    for (let i=0; i<checkboxes.length; i++){
         if (checkboxes[i].checked) {
-            return true
-        }
-    }
-    //If all checkboxes have been iterated through
-        //(which means they are all unchecked), returns false.
-    return false
-}
+            checkboxes.removeAttribute('required')
+        } //end conditional
+    } // end for loop
+}; //end function
+
+reqCheckOne()
+
+// not working
+// ERROR in CONSOLE:
+    // (1)
+    // p://localhost:3001/public/js/checkbox.js net::ERR_ABORTED 404 (Not Found)
+    //(2)
+    //Refused to execute script from 'http://localhost:3001/public/js/checkbox.js' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.
+
+//POSSIBLE SOLUTION?
+    // might need to create a custom event
+    // https://vanillajstoolkit.com/reference/event-listeners/customevent/
