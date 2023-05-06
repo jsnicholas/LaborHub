d4f6c05ecafcb5bad1e5956837ae7df353b355f1
Victoria <vmcnorrill@gmail.com>
2023-05-06 15:09:01 -0400

    wrote basic logic for function reqCheckOne()

--- public/js/checkbox.js
+++ public/js/checkbox.js
@@ -1,3 +1,17 @@
 // if user checks at least one input from the list,
 // then all "required" attr are removed
 
+let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+
+function reqCheckOne(){
+    // if a checkbox is checked, function ends and returns true.
+    for (let i = 0; i < checkboxes.length; i++){
+        if (checkboxes[i].checked) {
+            return true
+        }
+    }
+    //If all checkboxes have been iterated through
+        //(which means they are all unchecked), returns false.
+    return false
+}
