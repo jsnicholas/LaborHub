a9134258a2ccf84b85d2473213db52be380cd192
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-05-07 12:05:24 -0400

    cleaned up db scripts

--- models/index.js
+++ models/index.js
@@ -7,12 +7,10 @@ module.exports = { User, Employee, Dates, PayHist };
 
 const { DataTypes } = require('sequelize');
 
-// searching by a username can pull and employee's contact and demo info
 User.hasOne(Employee, {
     foreignKey: 'user_id'
 });
 
-// searching by an EE's name or phone # can pull an EE's login info
 Employee.belongsTo(User, {
     foreignKey: 'user_id'
 });
@@ -29,20 +27,3 @@ Dates.hasMany(PayHist, {
     foreignKey: 'date_id'
 });
 
-// PayHist.hasMany(Dates, {
-//     foreignKey: 'due'
-// })
-// Dates.belongsToMany(PayHist)
-
-// PayHist.belongsTo(Dates);
-
-// Dates.belongsToMany(PayHist, {
-//     through: 'payments',
-//     foreignKey: 'date_id',
-//     targetKey: 'id'
-// });
-
-// PayHist.belongsToMany(Dates, {
-    //     through: 'payments',
-//     foreignKey: 'date_id'
-// });
