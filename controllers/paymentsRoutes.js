629a19c65981d46269903642c990bb6042799702
John Kersey <johnkersey2@yahoo.com>
2023-05-07 16:59:59 -0400

    Progress for others to assist.

--- controllers/paymentsRoutes.js
+++ controllers/paymentsRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { User, Employee, PayHist } = require('../models');
-const withAuth = require('../utils/auth')
+// const withAuth = require('../utils/auth')
 
-router.get('/', withAuth, (req, res) => {
+router.get('/', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
+        res.render('payments');        
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const statesUS = require('./utils/json/states_hash.json')
-// const __dirname = path.resolve()
-// import fetch from "node-fetch";
+const __dirname = path.resolve()
+import fetch from "node-fetch";
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -51,84 +51,84 @@ sequelize.sync(
   app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
 });
 
-// // Paypal Integration
-// const payApp = express();
-// const getAccessToken = async () => {
-//   const clientId = "<test>";
-//   const appSecret = "<test_secret>";
-//   const url = "https://api-m.sandbox.paypal.com/v1/oauth2/token";
-//   const response = await fetch(url, {
-//     body: "grant_type=client_credentials",
-//     method: "POST",
-//     headers: {
-//       Authorization:
-//         "Basic " + Buffer.from(clientId + ":" + appSecret).toString("base64"),
-//     },
-//   });
-//   const data = await response.json();
-//   return data.access_token;
-// };
+// Paypal Integration
+const payApp = express();
+const getAccessToken = async () => {
+  const clientId = "<test>";
+  const appSecret = "<test_secret>";
+  const url = "https://api-m.sandbox.paypal.com/v1/oauth2/token";
+  const response = await fetch(url, {
+    body: "grant_type=client_credentials",
+    method: "POST",
+    headers: {
+      Authorization:
+        "Basic " + Buffer.from(clientId + ":" + appSecret).toString("base64"),
+    },
+  });
+  const data = await response.json();
+  return data.access_token;
+};
 
-// const createOrder = async () => {
-//   const url = "https://api-m.sandbox.paypal.com/v2/checkout/orders";
-//   const payload = {
-//     intent: "CAPTURE",
-//     purchase_units: [
-//       {
-//         amount: {
-//           currency_code: "USD",
-//           value: "0.02",
-//         },
-//       },
-//     ],
-//   };
-//   const headers = {
-//     Authorization: `Bearer ${await getAccessToken()}`,
-//     "Content-Type": "application/json",
-//   };
-//   const response = await fetch(url, {
-//     headers,
-//     method: "POST",
-//     body: JSON.stringify(payload),
-//   });
-//   const data = await response.json();
-//   if (data.error) {
-//     throw new Error(error);
-//   }
-//   return data;
-// };
+const createOrder = async () => {
+  const url = "https://api-m.sandbox.paypal.com/v2/checkout/orders";
+  const payload = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: "0.02",
+        },
+      },
+    ],
+  };
+  const headers = {
+    Authorization: `Bearer ${await getAccessToken()}`,
+    "Content-Type": "application/json",
+  };
+  const response = await fetch(url, {
+    headers,
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+  const data = await response.json();
+  if (data.error) {
+    throw new Error(error);
+  }
+  return data;
+};
 
-// const capturePayment = async (orderID) => {
-//   const url = `https://api-m.sandbox.paypal.com/v2/checkout/orders/${orderID}/capture`;
-//   const headers = {
-//     Authorization: `Bearer ${await getAccessToken()}`,
-//     "Content-Type": "application/json",
-//   };
-//   const response = await fetch(url, {
-//     headers,
-//     method: "POST",
-//   });
-//   const data = await response.json();
-//   if (data.error) {
-//     throw new Error(error);
-//   }
-//   return data;
-// };
+const capturePayment = async (orderID) => {
+  const url = `https://api-m.sandbox.paypal.com/v2/checkout/orders/${orderID}/capture`;
+  const headers = {
+    Authorization: `Bearer ${await getAccessToken()}`,
+    "Content-Type": "application/json",
+  };
+  const response = await fetch(url, {
+    headers,
+    method: "POST",
+  });
+  const data = await response.json();
+  if (data.error) {
+    throw new Error(error);
+  }
+  return data;
+};
 
-// payApp.get("/", (req, res) => {
-//   res.sendFile(`${__dirname}/index.html`);
-// });
+payApp.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
-// payApp.post("/orders", async (req, res) => {
-//   const response = await createOrder();
-//   res.json(response);
-// });
+payApp.post("/orders", async (req, res) => {
+  const response = await createOrder();
+  res.json(response);
+});
 
-// payApp.post("/orders/:orderID/capture", async (req, res) => {
-//   const response = await capturePayment(req.params.orderID);
-//   res.json(response);
-// });
+payApp.post("/orders/:orderID/capture", async (req, res) => {
+  const response = await capturePayment(req.params.orderID);
+  res.json(response);
+});
 
-// payApp.listen(9597, () => {
-//   console.log("listening on http://localhost:9597/");
-// });
+payApp.listen(9597, () => {
+  console.log("listening on http://localhost:9597/");
+});
